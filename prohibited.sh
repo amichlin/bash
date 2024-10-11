7f52b1477d7c3d5c5380f296158a827e8508c464
amichlin <amichlin@gmail.com>
2024-10-11 10:00:31 -0400

    Update prohibited.sh

--- prohibited.sh
+++ prohibited.sh
@@ -3,7 +3,7 @@
 # This script checks for Wireshark or ophcrack and removes them if they are installed
 
 # List of insecure software to check
-insecure_software=("wireshark" "ophcrack" "aisleriot" "ettercap-text-only" "ettercap-graphical")
+insecure_software=("wireshark" "ophcrack" "aisleriot" "ettercap-text-only" "ettercap-graphical" "nginx")
 
 for software in "${insecure_software[@]}"; do
   if dpkg -l | grep -q "^ii  $software"; then
